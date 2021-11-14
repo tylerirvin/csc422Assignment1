8186101c7b7c67ce1af6c23bfe83cd52e381d0c7
tj_ir <tj_ir@DESKTOP-G74I39B.lan>
2021-11-14 13:22:50 -0600

    Verify connections

--- CSC422Assignment1.java
+++ CSC422Assignment1.java
@@ -7,7 +7,7 @@ public class CSC422Assignment1
   public static void main(String[] args)
   {
       System.out.println("Pet Database Program.");
-      menu();
+      menu();  /* something */
     
   } /* End main() */
   
