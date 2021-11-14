7b769ce227f997c9650a9f29968225755ff4169b
tj_ir <tj_ir@DESKTOP-G74I39B.lan>
2021-11-14 15:35:40 -0600

    Added image of project board.

--- Pet.java
+++ Pet.java
@@ -1,4 +1,8 @@
-public class Pet {
+import java.io.Serializable;
+
+public class Pet implements Serializable {
+    private static final long serialVersionUID = 6529685098266757691L;   /* Serial match point */
+    
     private String name;
     private int age;
     
@@ -6,31 +10,36 @@ public class Pet {
     {
         this.name = name;
         this.age = age;
-    }
+    } /* End constructor */
     
+/* SETTERS */    
     public void setName(String name)
     {
         this.name = name;
-    }
+    } /* End setName(String) */
+    
     
     public void setAge(int age)
     {
         this.age = age;
-    }
+    } /*End setAge(int) */
+    
     
     public void updatePet(String name, int age)
     {
         this.name = name;
         this.age = age;
-    }
+    } /* End updatePet (name, age) */
     
+/* GETTERS */
     public String getName()
     {
         return name;
-    }
+    } /* End getName() */
+    
     
     public int getAge()
     {
         return age;
-    }
+    } /* End getAge() */
 }
